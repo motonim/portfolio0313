4d661c956ea7bc45a711886377e4f43b41a7ac1e
Jaeri Park <motonimjrp@gmail.com>
2021-07-21 23:20:36 -0400

    change portfolio section

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+$('.slider').slick({
+  centerMode: true,
+  centerPadding: '200px',
+  slidesToShow: 1,
+  focusOnSelect: true,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+// $('.slider').slick({
+//   centerMode: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 1500,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 3,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//       },
+//     },
+//   ],
+// });
+
+// window.addEventListener("scroll", function(){
+//     var nav = document.querySelector(".nav");
+//     nav.classList.toggle("sticky", window)
+// });
+
+$('.hamburger-menu').on('click', function () {
+  $(this).toggleClass('close');
+  $('.nav-list').toggleClass('show');
+});
